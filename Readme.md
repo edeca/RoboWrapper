fa837da2dbab0e46f921f39e226dec36647a3e7b
David Cannings <david.cannings@nccgroup.com>
2015-12-27 21:14:26 +0000

    Add initial version from internal repo

---
+++ Readme.md
@@ -0,0 +1,167 @@
+# RoboWrapper manual #
+
+RoboWrapper is a simple Python script that executes [Robocopy](https://en.wikipedia.org/wiki/Robocopy) with simple rule files.  These rule files allow auto-discovery of drive names and safety checks.
+
+RoboWrapper is only designed to run on Windows.
+
+If you were instead looking for robot rapping [see Youtube](https://www.youtube.com/watch?v=mvrva8NoMLM).
+
+## Why? ##
+
+I wrote primarily this because drive letters on Windows can change regularly unless fixed.  Sample use cases include:
+
+ * Copying files to a USB stick that changes drive letter each time it is inserted. 
+ * Syncing files to multiple USB sticks (where all have the same volume label).
+ * Copying files from multiple computers to a network drive.
+
+## Usage ##
+
+For help, run: `python RoboWrapper.py --help`.
+
+To run a job, run: `python RoboWrapper.py job.yaml` (add `-v` for debug).
+
+To list drives, run: `python RoboWrapper.py --drives`.
+
+## Basic configuration examples ##
+
+The most basic configuration needs a name and source / destination paths:
+
+```
+name: Backup some stuff
+source:
+    path: 'C:\MyStuff'
+destination:
+    path: 'E:\Backup\MyStuff'
+```
+
+However this isn't very useful if the USB drive letter changes.  A more robust configuration would use drive serial numbers:
+
+```
+name: Backup some stuff
+source:
+    serial: '70DC81A0' # OS drive
+    path: 'MyStuff'
+destination:
+    serial: '2C724B02' # Removable USB
+    path: 'Backup\MyStuff'
+```
+
+Robocopy options can also be specified which will be passed when copying:
+
+```
+name: Backup some stuff
+source:
+    serial: '70DC81A0' # OS drive
+    path: 'MyStuff'
+destination:
+    serial: '2C724B02' # Removable USB
+    path: 'Backup\MyStuff'
+robocopy:
+	# Warning, /MIR can delete files!
+    options: "/FFT /DST /MIR /XJ /NP /R:3 /W:30"
+```
+
+## Advanced configuration examples ##
+
+### Copying to a specific volume name ###
+
+Sometimes an exact serial may not be desirable.  For example the configuration below will copy to the first USB storage drive named "KINGSTON":
+
+```
+name: Sync to any drive named KINGSTON
+source:
+    serial: '70DC81A0' # OS drive
+    path: 'MyStuff'
+destination:
+    # Note the first drive found is used.  If serial is also
+    # specified then this will take precedence (if found).
+    label: 'KINGSTON'
+    path: 'Backup\MyStuff'
+```
+
+This is useful to duplicate the same files to a number of different storage devices which are plugged in one at a time, or if multiple users all have their own USB drive named "BACKUP".
+
+### Checking a safety flag ###
+
+To ensure files and folders are not overwritten by mistake an optional flag can be specified.  This should be a file (with any contents, or empty) which is checked before Robocopy is executed.
+
+In the example below the files `C:\MyStuff\Robocopy.flag` and `E:\Robocopy.flag` must be present or Robocopy will not be run.
+
+```
+name: Backup my stuff, check a flag first
+source:
+    serial: '70DC81A0'     # OS drive
+    path: 'MyStuff'        # Works out to C:\MyStuff\
+    flag: '$path$\Robocopy.flag'
+destination:
+    serial: '2C724B02'     # Removable USB
+    path: 'Backup\MyStuff' # Works out to E:\Backup\MyStuff\
+    flag: '$drive$\Robocopy.flag'
+```
+
+The special variables `$drive$` and `$path$` will be substituted with the drive and path found by serial number or label.
+
+### Using Windows environment variables ###
+
+RoboWrapper will expand a number of Windows environment variables automatically.  The variables should be surrounded by dollar signs like the example below.
+
+```
+name: Backup AppData
+source:
+    path: '$APPDATA$'
+destination:
+    serial: '2C724B02' # Removable USB
+    path: 'Backup\$COMPUTERNAME$\AppData'
+```
+
+Expansion in this way is preferable to the usual `%AppData%` format because Python can check the relevant directories exist before executing Robocopy.
+
+### Adding a Robocopy log ###
+
+The Robocopy log can be specified like below:
+
+```
+name: Backup some stuff
+source:
+    path: 'C:\MyStuff'
+destination:
+    path: 'E:\Backup\MyStuff'
+robocopy:
+    # Tell Robocopy to save a log
+    log: '$dst_drive$\Robocopy-$timestamp$.log'
+settings:
+    # Optional format parameter for $timestamp$
+    time_format: "%Y-%m-%dT%H%M%S"
+```
+
+The variables `$dst_drive$`, `$src_drive$`, `$dst_path$` and `$src_path$` will be expanded with the correct values.  `$timestamp$` will be substituted with the current time, which will be seconds since the epoch or may be formatted by strftime() using `time_format` (see [format strings](https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior)).
+
+## FAQ ##
+
+### How can I test a job? ###
+
+Add the `--dry-run` parameter when calling the script.  This will go through all the same steps of resolving drive letters, substituting environment variables and checking paths but will not execute Robocopy.
+
+Run with `-v` for debug output.
+
+### How can I get drive serial numbers? ###
+
+Call the script with the `--drives` option which will display a table like below.
+
+```
++-------+----------+------+
+| Drive |  Serial  | Name |
++-------+----------+------+
+|   C:  | 70DC81A0 |      |
+|   D:  |   None   | None |
+|   E:  | 2C724B02 | data |
++-------+----------+------+
+```
+
+## Requirements ##
+
+The script requires [pywin32](http://sourceforge.net/projects/pywin32/) which needs to be installed using the setup wizard.
+
+A number of other dependencies are also required, these can be installed using pip:
+
+`pip install -r Requirements.txt` 
---
+++ RoboWrapper.py
@@ -0,0 +1,455 @@
+import win32com.client
+import os.path
+import subprocess
+import sys
+import yaml
+import pytz
+import argparse
+import errno
+import glob
+import logging
+from datetime import datetime
+from prettytable import PrettyTable
+
+########
+# Author: David <david@edeca.net>
+#   Date: March 2014
+#
+# This script is a simple wrapper around Robocopy, intended to assist
+# with drive letters that move around (e.g. USB drives, CD, network
+# shares).
+#
+# Jobs are defined as simple YAML documents which describe the source
+# and destination, Robocopy options and an optional log.
+#
+# See README.HTML for installation, configuration and usage details.
+########
+
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+BANNER = """
+ ____       _        __        __
+|  _ \ ___ | |__   __\ \      / / __ __ _ _ __  _ __   ___ _ __
+| |_) / _ \| '_ \ / _ \ \ /\ / / '__/ _` | '_ \| '_ \ / _ \ '__|
+|  _ < (_) | |_) | (_) \ V  V /| | | (_| | |_) | |_) |  __/ |
+|_| \_\___/|_.__/ \___/ \_/\_/ |_|  \__,_| .__/| .__/ \___|_|
+             by David Cannings (@edeca)  |_|   |_|     v{}.{}
+"""
+
+# Environment variables which will be expanded
+VALID_ENV_VARS = ['SYSTEMROOT', 'TMP', 'COMPUTERNAME', 'USERDOMAIN',
+        'PROGRAMFILES', 'PROGRAMFILES(X86)', 'COMMONPROGRAMFILES(X86)',
+        'ALLUSERSPROFILE', 'LOCALAPPDATA', 'HOMEPATH', 'PROGRAMW6432',
+        'USERNAME', 'PROGRAMDATA', 'WINDIR', 'APPDATA', 'HOMEDRIVE',
+        'SYSTEMDRIVE', 'COMMONPROGRAMW6432', 'PUBLIC', 'USERPROFILE']
+
+# Cache first WMI call as it can be slow
+wmiLogicalDisks = None
+
+
+class RoboException(Exception):
+    pass
+
+
+def GetLogicalDrivesFromWMI():
+    global wmiLogicalDisks
+
+    # Speedup: underlying WMI calls only made once then cached
+    if wmiLogicalDisks is None:
+        strComputer = "."
+        objWMIService = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+        objSWbemServices = objWMIService.ConnectServer(strComputer,
+            "root\cimv2")
+        wmiLogicalDisks = objSWbemServices.ExecQuery(
+            "Select * from Win32_LogicalDisk")
+
+    return wmiLogicalDisks
+
+
+def FindDriveFromSerial(serial):
+    """ Find drive letter from a serial number (e.g. 'ABCD1234') """
+
+    for objItem in GetLogicalDrivesFromWMI():
+        if objItem.VolumeSerialNumber == serial:
+            return objItem.Name
+
+    return None
+
+
+def FindDriveFromName(name):
+    """ Find drive letter from volume name (e.g. 'KINGSTON') """
+
+    for objItem in GetLogicalDrivesFromWMI():
+        if objItem.VolumeName == name:
+            return objItem.Name
+
+    return None
+
+
+def ResolveDriveLetter(options):
+    """ Find drive letter from source or destination settings """
+
+    tried = []
+
+    # The first likely match is returned
+    if 'serial' in options:
+        tried.append("serial: {}".format(options['serial']))
+
+        found = FindDriveFromSerial(options['serial'])
+        if found:
+            return found
+
+    if 'name' in options:
+        tried.append("name: {}".format(options['name']))
+
+        found = FindDriveFromName(options['name'])
+        if found:
+            return found
+
+    raise RoboException("Couldn't find drive letter, tried: {}".format(
+        ", ".join(tried)))
+
+
+def ListDrives():
+    """ Print a table of currently available drives """
+
+    table = PrettyTable(["Drive", "Serial", "Name"])
+    for objItem in GetLogicalDrivesFromWMI():
+        table.add_row([objItem.Name, objItem.VolumeSerialNumber,
+                objItem.VolumeName])
+
+    print table
+
+
+def CheckFlag(options, path):
+    """ Check the 'safety' flag and ensure it exists """
+
+    # Flag is optional, return success if not specified
+    if 'flag' not in options:
+        return True
+
+    flag = options['flag']
+    flag = flag.replace('$drive$', os.path.splitdrive(path)[0])
+    # It makes little sense to use $path$ with Robocopy's /MIR option,
+    # which deletes files.  The current log won't be deleted (because
+    # Robocopy will have the file open) but older logs will be removed.
+    flag = flag.replace('$path$', path)
+
+    logging.debug("Checking flag: {} ".format(flag))
+
+    return os.path.isfile(flag)
+
+
+def DoRobocopy(job, dry_run):
+    """ Call robocopy and parse exit status for success """
+
+    cmd = []
+    cmd.append('robocopy')
+    cmd.append(job['run']['src_path'])
+    cmd.append(job['run']['dst_path'])
+    cmd.append(job['run']['file_types'])
+    cmd.extend(job['run']['options'])
+
+    logging.debug("Command: {}".format(" ".join(cmd)))
+
+    if dry_run:
+        return True
+
+    # TODO: If debug verbosity is set then don't discard output
+    DEVNULL = open(os.devnull, 'wb')
+    proc = subprocess.Popen(cmd, stdout=DEVNULL, stderr=DEVNULL)
+
+    logging.debug("Robocopy running (PID: {})".format(proc.pid))
+
+    # TODO: Allow user to provide an optional timeout and kill?
+    proc.wait()
+
+    logging.debug("Robocopy finished, return code: {}".format(proc.returncode))
+
+    # TODO: Parse status code fully (see http://ss64.com/nt/robocopy-exit.html)
+    if proc.returncode >= 8:
+        return False
+
+    return True
+
+
+def ValidateJob(job):
+    """ Validate loaded job for required settings """
+
+    if 'name' not in job:
+        raise RoboException("A name is required")
+
+    if 'source' not in job:
+        raise RoboException("Source settings missing!")
+
+    if 'destination' not in job:
+        raise RoboException("Destination settings missing!")
+
+    if 'path' not in job['source']:
+        raise RoboException("Source path not defined")
+
+    if 'path' not in job['destination']:
+        raise RoboException("Destination path not defined")
+
+    # TODO: Check serial, should be 8 hex characters
+
+    return True
+
+
+def ExpandEnvironmentVars(path):
+    """ Expand environment variables """
+    for var in VALID_ENV_VARS:
+        try:
+            path = path.replace("${}$".format(var), os.environ[var])
+        except KeyError:
+            pass
+
+    return path
+
+
+def SubstitutePath(path, job):
+    """ Substitute placeholders with derived values """
+
+    path = path.replace("$src_path$", job['run']['src_path'])
+    path = path.replace("$dst_path$", job['run']['dst_path'])
+    path = path.replace("$src_drive$", job['run']['src_drive'])
+    path = path.replace("$dst_drive$", job['run']['dst_drive'])
+
+    # Format timestamp
+    timestamp = job['run']['time'].strftime(job['run']['time_format'])
+    path = path.replace("$timestamp$", timestamp)
+
+    path = ExpandEnvironmentVars(path)
+
+    return path
+
+
+def ParseSettings(job):
+    """ Parse generic job options """
+
+    # Optional setting: Extract the timestamp format (used for logs)
+    try:
+        job['run']['time_format'] = job['settings']['time_format']
+    except KeyError:
+        pass
+
+    return job
+
+
+def ParseRobocopyOptions(job):
+    """ Parse Robocopy options (all optional) """
+
+    # Optional setting: Robocopy settings
+    try:
+        job['run']['options'] = job['robocopy']['options'].split(' ')
+    except KeyError:
+        pass
+
+    # Optional setting: Output file for Robocopy logs
+    try:
+        log_path = SubstitutePath(job['robocopy']['log'], job)
+        logging.debug("Log will be saved to: {}".format(log_path))
+        job['run']['options'].append("/LOG:{}".format(log_path))
+    except KeyError:
+        pass
+
+    # Optional setting: Robocopy files to copy
+    try:
+        # Parse out the files option, should be a glob of extensions like "*.*"
+        job['run']['file_types'] = job['robocopy']['files']
+    except KeyError:
+        pass
+
+    return job
+
+
+def ParseLocations(job):
+    """ Find source and destination paths for a job """
+
+    # Strategy is: find drive letter, expand path, check it exists
+
+    job['run']['src_path'] = ExpandEnvironmentVars(job['source']['path'])
+    job['run']['dst_path'] = ExpandEnvironmentVars(job['destination']['path'])
+
+    # These keys trigger drive letter lookup
+    k = ['serial', 'name']
+    if any(name in k for name in job['source'].keys()):
+        job['run']['src_drive'] = ResolveDriveLetter(job['source'])
+        job['run']['src_path'] = os.path.join(job['run']['src_drive'],
+                '\\', job['run']['src_path'])
+
+    # Otherwise we need to derive the drive letter from the path
+    else:
+        job['run']['src_drive'] = os.path.splitdrive(job['run']['src_path'])[0]
+
+    # TODO: Remove duplicated code.  This will require refactoring job['run']
+    #       into source and destination sections
+    if any(name in k for name in job['destination'].keys()):
+        job['run']['dst_drive'] = ResolveDriveLetter(job['destination'])
+        job['run']['dst_path'] = os.path.join(job['run']['dst_drive'],
+                '\\', job['run']['dst_path'])
+
+    # Otherwise we need to derive the drive letter from the path
+    else:
+        job['run']['dst_drive'] = os.path.splitdrive(job['run']['dst_path'])[0]
+
+    # Check source path exists.  Destination path may not exist yet but can
+    # be created by Robocopy.
+    if not os.path.exists(job['run']['src_path']):
+        raise RoboException("Source doesn't exist: {}".format(
+                job['run']['src_path']))
+
+    return job
+
+
+def DefaultJobSettings(job):
+    """ Setup default options which can be replaced with per-job settings """
+
+    job['run'] = dict()
+
+    # TODO: Read default options from somewhere to allow setting of
+    #       these globally for all jobs, without specifying in every
+    #       job file
+    job['run']['options'] = []
+    job['run']['file_types'] = "*.*"
+    # Default is similar to UTC format without colons (invalid on Windows)
+    job['run']['time_format'] = "%Y-%m-%dT%H%M%S%z"
+    job['run']['time'] = pytz.utc.localize(datetime.now())
+
+    return job
+
+
+def LoadJob(fp):
+    """ Load YAML from a stream and handle any errors """
+
+    try:
+        job = yaml.load(fp)
+    except yaml.YAMLError, exc:
+        logging.error("YAML error in job file {} (run with -v for debug)"
+                .format(fp.name))
+        logging.debug("YAML parser output: {}".format(exc))
+        return None
+
+    return job
+
+
+def RunJob(job_file, dry_run=False):
+    # Check job is a file
+    if not os.path.isfile(job_file):
+        logging.error("Could not find file {}".format(job_file))
+        return False
+
+    # Load and parse YAML data
+    try:
+        fp = open(job_file, 'r')
+    except IOError as e:
+        # Check for permissions problems in a safe way
+        if e.errno == errno.EACCES:
+            logging.error("Could not access file {} (permission denied)"
+                    .format(job_file))
+            return False
+        raise
+    else:
+        with fp:
+            job = LoadJob(fp)
+            if job is None:
+                return False
+
+    try:
+        ValidateJob(job)
+        logging.debug("Loaded job: {}".format(job['name']))
+    except RoboException, exc:
+        logging.error("Configuration problem: {} (in {})"
+                .format(exc, job_file))
+        return False
+
+    try:
+        job = DefaultJobSettings(job)
+        job = ParseSettings(job)
+        job = ParseLocations(job)
+        job = ParseRobocopyOptions(job)
+
+    except RoboException, exc:
+        logging.error("{}".format(exc))
+        return False
+
+    # Check for safety flags in drives
+    # TODO: Move into a generic JobReady() function
+    if not CheckFlag(job['source'], job['run']['src_drive']):
+        logging.error("Could not find source safety flag")
+        return False
+
+    if not CheckFlag(job['destination'], job['run']['dst_drive']):
+        logging.error("Could not find destination safety flag")
+        return False
+
+    return DoRobocopy(job, dry_run)
+
+
+def main():
+    succeeded = 0
+    failed = 0
+
+    logging.basicConfig(format='%(levelname)8s: %(message)s')
+
+    parser = argparse.ArgumentParser(description=
+            'Run robocopy in a world of changing drive letters.')
+    parser.add_argument('job', nargs='*',
+        help="job files to run (can be a glob like *.yaml)")
+    parser.add_argument('-d', '--dry-run', dest='dry_run', action='store_true',
+        default=False, help="test jobs but don't run robocopy")
+    parser.add_argument('-v', '--verbose', dest="verbosity", action='count',
+        default=0, help="enable debug output")
+    parser.add_argument('--drives', dest='drives', action='store_true',
+        default=None, help="list information about available drives")
+
+    args = parser.parse_args()
+
+    print BANNER.format(VERSION_MAJOR, VERSION_MINOR)
+
+    # Each -v option removes 10 from the default level of INFO
+    level = 20 - args.verbosity * 10
+    logging.getLogger('').setLevel(level)
+
+    if args.drives:
+        ListDrives()
+        sys.exit(0)
+
+    if not len(args.job):
+        logging.error("Need at least one job (or --drives), see --help")
+        sys.exit(1)
+
+    # Don't allow multiple file globs to find the same job file twice
+    jobs = set()
+
+    for path in args.job:
+        for file in glob.glob(path):
+            jobs.add(file)
+
+    logging.info("Found {} job(s) to run".format(len(jobs)))
+
+    if args.dry_run:
+        logging.warning("--dry-run option given, won't execute Robocopy")
+
+    for job in jobs:
+        if file.endswith(".yaml"):
+            logging.info("Running job from {}".format(job))
+            
+            if RunJob(job, dry_run=args.dry_run):
+                logging.debug("Job status OK {}".format(job))
+                succeeded += 1
+            else:
+                logging.warning("Job status failed {}".format(job))
+                failed += 1
+        else:
+            logging.warning("Ignoring job with incorrect extension: {}"
+                    .format(job))
+
+    logging.info("Finished: {} succeeded and {} failed"
+            .format(succeeded, failed))
+
+    # TODO: Option to eject/safely remove afterward?
+
+
+if __name__ == "__main__":
+    main()
